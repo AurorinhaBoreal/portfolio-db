cca65a9ddd3712e6a4eb7af2815b256f6a8b57d2
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-07-31 13:13:04 -0300

    feat: Implement Stylization

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import theme from './styles/theme';
+import "./styles/main.css"
 import Home from './pages/Home'
 import { ChakraProvider } from '@chakra-ui/react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
@@ -22,7 +24,7 @@ const router = createBrowserRouter([
 ])
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
