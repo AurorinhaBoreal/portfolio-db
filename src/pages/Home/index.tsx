0d3240d68a82048fa4385912a6e8ad1ee7f6456b
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-01 16:26:03 -0300

    feat: Implement Home Structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,27 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Image, Text } from "@chakra-ui/react"
 import styles from "./h.module.css"
 import Header from "../../components/Header"
+import myImage from "../../assets/myImage.jpg"
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Box className={styles.bodyContainer}>
-
-      </Box>
+        <Box className={styles.bodyContainer}>
+          <Box className={styles.infoWrapper}>
+              <Text className={styles.introText}>Olá, meu nome é</Text>
+              <Text className={styles.myName}>Aurora Kruschewsky</Text>
+              <Text className={styles.role}>Sou desenvolvedora FullStack na DBServer</Text>
+          </Box>
+          <Image src={myImage} className={styles.myImage}/>
+          <Box className={styles.aboutWrapper}>
+            <Text className={styles.aboutTitle}>Sobre mim</Text>
+            <Text className={styles.aboutInfo}>Texto que me descreve :D</Text>
+          </Box>
+          <Box className={styles.skillWrapper}>
+            <Text className={styles.skillTitle}>Habilidades:</Text>
+          </Box>
+        </Box>
     </>
   )
 }
