ddef29bb486cab82b66cf23ac9b27a34afb5670f
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-01 16:25:48 -0300

    refactor: Change Tab Icon and Implement CV in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,30 @@
 import { Image, Text, Flex } from "@chakra-ui/react"
 import styles from "./h.module.css"
+import cv from "../../assets/Aurora_Curriculo.pdf"
+import dbHeader from "../../assets/header-DB.png"
 import githubImg from "../../assets/github.svg"
 import linkedinImg from "../../assets/linkedin.svg"
+import { Link } from "react-router-dom"
 
 export default function Header() {
   const LINKEDIN = () => window.open("https://www.linkedin.com/in/aurora-kruschewsky")
   const GITHUB = () => window.open("https://github.com/AurorinhaBoreal")
   const DB = () => window.open("https://db.tec.br/")
+  const CV = () => window.open(cv)
 
   return (
     <Flex className={styles.headerBody}>
       <Flex className={styles.logo_textWrapper}>
         <Flex className={styles.logoContainer}>
-          <Image src="/Logo-DB.png" onClick={DB} _hover={{ cursor: "pointer"}}/>
+          <Image src={dbHeader} onClick={DB} _hover={{ cursor: "pointer"}}/>
         </Flex>
-        <Text>Sobre</Text>
-        <Text>Projetos</Text>
-        <Text>Curriculo</Text>
+        <Link to={"/projects"}>
+          <Text>Projetos</Text>
+        </Link>
+        <Link to={"/home"}>
+          <Text>Sobre</Text>
+        </Link>
+        <Text onClick={CV} _hover={{ cursor: "pointer"}}>Curriculo</Text>
       </Flex>
       <Flex className={styles.iconsWrapper}>
         <Image className={styles.icons} src={githubImg} onClick={GITHUB} _hover={{ cursor: "pointer"}}/>
