7f16ddf79cf7b3ed86347fc2d6fbfd336da60738
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-01 11:50:23 -0300

    feat: Create Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,27 @@
-import { Box, Text } from "@chakra-ui/react"
+import { Image, Text, Flex } from "@chakra-ui/react"
 import styles from "./h.module.css"
+import githubImg from "../../assets/github.svg"
+import linkedinImg from "../../assets/linkedin.svg"
 
 export default function Header() {
+  const LINKEDIN = () => window.open("https://www.linkedin.com/in/aurora-kruschewsky")
+  const GITHUB = () => window.open("https://github.com/AurorinhaBoreal")
+  const DB = () => window.open("https://db.tec.br/")
+
   return (
-    <Box className={styles.headerBody}>
-      <Box className={styles.topicWrapper}>
+    <Flex className={styles.headerBody}>
+      <Flex className={styles.logo_textWrapper}>
+        <Flex className={styles.logoContainer}>
+          <Image src="/Logo-DB.png" onClick={DB} _hover={{ cursor: "pointer"}}/>
+        </Flex>
         <Text>Sobre</Text>
         <Text>Projetos</Text>
         <Text>Curriculo</Text>
-      </Box>
-    </Box>
+      </Flex>
+      <Flex className={styles.iconsWrapper}>
+        <Image className={styles.icons} src={githubImg} onClick={GITHUB} _hover={{ cursor: "pointer"}}/>
+        <Image className={styles.icons} src={linkedinImg} onClick={LINKEDIN} _hover={{ cursor: "pointer"}}/>
+      </Flex>
+    </Flex>
   )
 }
