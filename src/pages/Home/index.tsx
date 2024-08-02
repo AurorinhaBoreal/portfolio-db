c60c86ad82a67859e0d92997f99ea81274e955db
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-02 15:33:09 -0300

    feat: Add Photo, Text and Implement Responsiveness

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,10 +14,23 @@ export default function Home() {
               <Text className={styles.myName}>Aurora Kruschewsky</Text>
               <Text className={styles.role}>Sou desenvolvedora FullStack na DBServer</Text>
           </Box>
-          <Image src={myImage} className={styles.myImage}/>
+          <Box display={"flex"}>
+            <Image src={myImage} className={styles.myImage}/>
+          </Box>
           <Box className={styles.aboutWrapper}>
             <Text className={styles.aboutTitle}>Sobre mim</Text>
-            <Text className={styles.aboutInfo}>Texto que me descreve :D</Text>
+            <Text className={styles.aboutInfo}>
+              Tive meu primeiro contato com a programação em 2021 no 2° Ano do Ensino Médio com Técnico em Automação Industrial na ETEC. Desde então me encantei pela área.
+            </Text>
+            <Text  className={styles.aboutInfo}>
+              Após fui em busca da minha vocação, durante o caminho fiz um Técnico em Redes de Computadores também na ETEC, o qual me deu uma boa base em infraestrutura e protocolos de comunicação. Tive também a oportunidade de ver outras linguagens de programação durante ele o que acabou me dando Certeza da área que eu queria seguir.
+            </Text>
+            <Text  className={styles.aboutInfo}>
+              No ultimo semestre do curso eu comecei o que eu me encontro atualmente que é o Técnico em Desenvolvimento e Sistemas. Nele entrei de cabeça no mundo da programação, começando pelo básico HTML e JavaScript e vendo diversas outras linguagens e frameworks, como C#, Node e React.
+            </Text>
+            <Text className={styles.aboutInfo}>
+              Atualmente meu foco está em Java e o maravilhoso React.
+            </Text>
           </Box>
           <Box className={styles.skillWrapper}>
             <Text className={styles.skillTitle}>Habilidades:</Text>
