da0af23b821565baeece3077e96381cfa6d833b4
Aurora Kruschewsky <AurorinhaBoreal@users.noreply.github.com>
2024-08-02 15:32:40 -0300

    feat: Implement SkillCard Component

--- src/components/SkillCard/index.tsx
+++ src/components/SkillCard/index.tsx
@@ -1,10 +1,40 @@
-import { Box } from "@chakra-ui/react"
+import { Flex, ListItem, UnorderedList, Image } from "@chakra-ui/react"
 import styles from "./sc.module.css"
+import {getSkillLevel, getSkillName } from "../../utils/skillInfo"
+import fillSkill from "../../assets/filledSkill.svg"
+import emptySkill from "../../assets/emptySkill.svg"
 
 export default function SkillCard() {
+  const skillNames = getSkillName()
+  const skillLevels = getSkillLevel()
+
+  const handleLevel = (full: number) => {
+    let skill = []
+    const empty = 5 - full
+    
+    for (let i = 1; i <= full; i++) {
+      skill.push(<Image key={"filled"+i} src={fillSkill}/>)
+    }
+
+    for (let i = 1; i <= empty; i++) {
+      skill.push(<Image key={"empty"+i} src={emptySkill}/>)
+    }
+    
+    return skill
+  }  
+
   return (
-    <Box className={styles.cardBody}>
-      index
-    </Box>
+    <>
+      {skillNames.map((skill, index) => (
+          <UnorderedList className={styles.cardWrapper}>
+            <Flex>
+              <ListItem className={styles.skillText}>{skill}</ListItem>
+            </Flex>
+            <Flex className={styles.dotWrapper}>
+              {handleLevel(skillLevels[index])}
+            </Flex>
+          </UnorderedList>
+      ))}
+    </>
   )
 }
